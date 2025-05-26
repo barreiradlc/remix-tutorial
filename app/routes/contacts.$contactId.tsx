4562d57234f1fbdab00d37fdce4909088d304d53
barreiradlc ( Augusto Barreira ) <barreira266@hotmail.com>
2025-05-26 10:55:31 -0300

    Load contact data

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,25 @@
-import { Form } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import { getContact } from "../data";
 
 import type { ContactRecord } from "../data";
 
+import invariant from "tiny-invariant";
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({
+  params,
+}: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+   if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return { contact };
+};
+
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
